d72d23120acbac01fab45fe72ae50ec5e53cc8b1
Dud4Mendes <129318829+Dud4Mendes@users.noreply.github.com>
2024-07-30 12:03:19 -0300

    Add files via upload

---
+++ nota.py
@@ -0,0 +1,8 @@
+nota01 = float(input("digite a nota do primeiro semestre: "))
+nota02 = float(input("digite a nota do segundo semestre: "))
+nota03 = float(input("digite a nota do terceiro semetre: "))
+nota04 = float(input("digite a nota do quarto semestre: "))
+
+media = (nota01 + nota02 + nota03 + nota04)/4
+
+print("a media final é: ", media)
